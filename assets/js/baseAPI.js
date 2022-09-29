4ce572ff11546cf922367ca060064af6730d972b
Jason <15237155725@163.com>
2022-09-29 15:54:29 +0800

    创建了login分支，并制作完成login.html

---
+++ assets/js/baseAPI.js
@@ -0,0 +1,20 @@
+$.ajaxPrefilter(function(config){
+
+// 封装一个函数
+function format2json(source) {
+  let target = {}
+  source.split('&').forEach(function (el) {
+    const [k1, k2] = el.split('=')
+    target[k1] = k2
+
+  })
+  return JSON.stringify(target)
+
+}
+
+config.url = 'http://big-event-vue-api-t.itheima.net'+config.url
+
+config.contentType = 'application/json'
+config.data = format2json(config.data)
+
+})
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,10 +1,87 @@
-$(function(){
-  $('#go2reg').on('click',function(){
+$(function () {
+  $('#go2reg').on('click', function () {
     $('.login-wrap').hide()
     $('.reg-wrap').show()
   })
-  $('#go2login').on('click',function(){
+  $('#go2login').on('click', function () {
     $('.login-wrap').show()
     $('.reg-wrap').hide()
   })
+
+
+  const form = layui.form
+  const layer = layui.layer
+  form.verify({
+    pwd: [
+      /^[\S]{6,12}$/
+      , '密码必须6到12位，且不能出现空格'
+    ],
+    repwd: function (value) {
+
+      const value1 = $('.reg-wrap [name="password"]').val()
+      if (value !== value1) {
+        return '两次密码不一致，请重新输入'
+      }
+    }
+  })
+
+  
+
+
+  $('#regForm').on('submit', function (e) {
+
+    e.preventDefault()
+    // console.log($('#regForm').serialize())
+// console.log(format2json($(this).serialize()));
+    $.ajax({
+      method: 'POST',
+      url: '/api/reg',
+      // contentType: 'application/json',
+      // data: JSON.stringify({
+      //   username: $('#regForm [name=username]').val(),
+      //   password: $('#regForm [name=password]').val(),
+      //   repassword: $('#regForm [name=repassword]').val()
+      // }),
+      data: $(this).serialize(),
+      success: function (res) {
+        console.log(res)
+        if (res.code !== 0) {
+          return layer.msg('注册用户失败' + res.message)
+        }
+        layer.msg(res.message)
+        $('#go2login').click()
+      }
+    })
+
+
+  })
+
+  $('#loginForm').submit(function(e){
+    e.preventDefault()
+    $.ajax({
+      method: 'POST',
+      url: '/api/login',
+      contentType: 'application/json',
+      // data: JSON.stringify({
+      //   username: $('#regForm [name=username]').val(),
+      //   password: $('#regForm [name=password]').val(),
+      //   repassword: $('#regForm [name=repassword]').val()
+      // }),
+      data: $(this).serialize(),
+      success: function (res) {
+        console.log(res)
+        if (res.code !== 0) {
+          return layer.msg(res.message)
+        }
+        layer.msg('登录成功')
+        // 把识别码存储到本地
+        localStorage.setItem('token',res.token)
+
+        // 跳转到主页
+        location.href='/index.html'
+      
+      }
+    })
+  })
+  
 })
