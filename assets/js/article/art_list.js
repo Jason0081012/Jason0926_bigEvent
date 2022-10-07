7b8b01a88f09ae5f3a1884f26815d77d01afe8c1
Jason <15237155725@163.com>
2022-10-07 18:39:20 +0800

    完成了部分文章列表

---
+++ assets/js/article/art_list.js
@@ -0,0 +1,27 @@
+$(function(){
+const layer = layui.layer
+
+  let  qs ={
+    pagenum:1,//当前页码值（表示当前第几页）
+    pagesize:2,//当前每页显示多少条
+    cate_id:'',//当前选择的文章分类
+    state:''  //当前文章所处的状态，可选值：已发布，草稿，都是字符串类型
+
+  }
+  
+  // 加载文章列表
+  loadArticleList()
+
+  function loadArticleList(){
+    $.ajax({
+      method:'GET',
+      url:`/my/article/list?pagenum=${qs.pagenum}&pagesize=${qs.pagesize}&cate_id=${qs.cate_id}&state=${qs.state}`,
+      success(res){
+        if(res.code!==0){
+          return layer.msg('获取文章列表失败')
+        }
+        console.log(res)
+      }
+    })
+  }
+})
